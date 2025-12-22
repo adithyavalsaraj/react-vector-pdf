6ee827922f70869d063da8dd1eab8958f8878790
Adithya Valsaraj <adithya@adivals.com>
2025-12-22 17:49:17 +0530

    chore: configure gh-pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,39 +3,48 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 
-export default defineConfig({
-  plugins: [
-    react(),
-    dts({
-      insertTypesEntry: true,
-      exclude: ["src/demo"],
-      rollupTypes: true,
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: '@use "@/styles/variables" as *;',
+export default defineConfig(({ mode }) => {
+  const isDemo = mode === "demo";
+
+  return {
+    base: isDemo ? "/react-vector-pdf/" : "/",
+    plugins: [
+      react(),
+      dts({
+        insertTypesEntry: true,
+        exclude: ["src/demo"],
+        rollupTypes: true,
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
       },
     },
-  },
-  build: {
-    lib: {
-      entry: fileURLToPath(new URL("./src/index.ts", import.meta.url)),
-      name: "ReactVectorPdf",
-      fileName: "react-vector-pdf",
-      formats: ["es"],
-    },
-    rollupOptions: {
-      external: ["react", "react-dom", "jspdf"],
-      output: {
-        globals: { react: "React", "react-dom": "ReactDOM", jspdf: "jsPDF" },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/styles/variables" as *;',
+        },
       },
     },
-  },
+    build: {
+      ...(isDemo
+        ? {}
+        : {
+            lib: {
+              entry: fileURLToPath(new URL("./src/index.ts", import.meta.url)),
+              name: "ReactVectorPdf",
+              fileName: "react-vector-pdf",
+              formats: ["es"],
+            },
+          }),
+      rollupOptions: {
+        external: isDemo ? [] : ["react", "react-dom", "jspdf"],
+        output: {
+          globals: { react: "React", "react-dom": "ReactDOM", jspdf: "jsPDF" },
+        },
+      },
+    },
+  };
 });
